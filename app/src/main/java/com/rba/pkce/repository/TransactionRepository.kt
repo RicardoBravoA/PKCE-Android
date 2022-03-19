9a7eea8a02d5cb556017329ea9a379c283ef1fef
Ricardo Bravo <ricardobravo@bcp.com.pe>
2022-03-19 00:30:54 -0500

    feat: add transaction repository

--- app/src/main/java/com/rba/pkce/repository/TransactionRepository.kt
+++ app/src/main/java/com/rba/pkce/repository/TransactionRepository.kt
@@ -1,11 +1,11 @@
 package com.rba.pkce.repository
 
 import com.rba.pkce.model.error.ErrorModel
-import com.rba.pkce.model.movie.Movie
+import com.rba.pkce.model.transaction.Transaction
 import com.rba.pkce.util.ResultType
 
 interface TransactionRepository {
 
-    suspend fun pay(): ResultType<Movie, ErrorModel>
+    suspend fun pay(): ResultType<Transaction, ErrorModel>
 
 }
