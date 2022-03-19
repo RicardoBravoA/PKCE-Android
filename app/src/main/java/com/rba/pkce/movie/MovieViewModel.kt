ec72aceff6d65897712d5284c67b6897df252b27
Ricardo Bravo <ricardobravo@bcp.com.pe>
2022-03-19 00:47:16 -0500

    feat: update view model

--- app/src/main/java/com/rba/pkce/movie/MovieViewModel.kt
+++ app/src/main/java/com/rba/pkce/movie/MovieViewModel.kt
@@ -4,20 +4,28 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.rba.pkce.model.request.transaction.TransactionRequest
 import com.rba.pkce.model.response.error.ErrorModel
 import com.rba.pkce.model.response.movie.Movie
+import com.rba.pkce.model.response.transaction.Transaction
 import com.rba.pkce.repository.MovieRepository
+import com.rba.pkce.repository.TransactionRepository
 import com.rba.pkce.util.ResultType
 import com.rba.pkce.util.SingleEvent
 import kotlinx.coroutines.launch
 
 class MovieViewModel(
-    private val movieRepository: MovieRepository
+    private val movieRepository: MovieRepository,
+    private val transactionRepository: TransactionRepository
 ): ViewModel() {
 
     private val _data = MutableLiveData<SingleEvent<List<Movie>>>()
     val data: LiveData<SingleEvent<List<Movie>>>
         get() = _data
+
+    private val _dataTransaction = MutableLiveData<SingleEvent<Transaction>>()
+    val dataTransaction: LiveData<SingleEvent<Transaction>>
+        get() = _dataTransaction
 
     private val _error = MutableLiveData<SingleEvent<String>>()
     val error: LiveData<SingleEvent<String>>
@@ -33,6 +41,24 @@ class MovieViewModel(
                 when (val result = movieRepository.get()) {
                     is ResultType.Success -> {
                         _data.value = SingleEvent(result.value)
+                    }
+                    is ResultType.Error -> {
+                        _apiError.value = SingleEvent(result.value)
+                    }
+                }
+            } catch (e: Exception) {
+                _error.value = SingleEvent(e.localizedMessage ?: "I'm an error")
+            }
+        }
+    }
+
+    fun pay(clientId: String, amount: Double) {
+        val request = TransactionRequest(clientId, amount)
+        viewModelScope.launch {
+            try {
+                when (val result = transactionRepository.pay(request)) {
+                    is ResultType.Success -> {
+                        _dataTransaction.value = SingleEvent(result.value)
                     }
                     is ResultType.Error -> {
                         _apiError.value = SingleEvent(result.value)
--- app/src/main/java/com/rba/pkce/movie/MovieViewModelFactory.kt
+++ app/src/main/java/com/rba/pkce/movie/MovieViewModelFactory.kt
@@ -3,13 +3,15 @@ package com.rba.pkce.movie
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 import com.rba.pkce.datasource.MovieDataSource
+import com.rba.pkce.datasource.TransactionDataSource
 
 @Suppress("UNCHECKED_CAST")
 class MovieViewModelFactory: ViewModelProvider.Factory {
     override fun <T : ViewModel?> create(modelClass: Class<T>): T {
         if (modelClass.isAssignableFrom(MovieViewModel::class.java)) {
             val movieDataSource = MovieDataSource()
-            return MovieViewModel(movieDataSource) as T
+            val transactionDataSource = TransactionDataSource()
+            return MovieViewModel(movieDataSource, transactionDataSource) as T
         }
         throw IllegalArgumentException("Unable to construct view model")
     }
